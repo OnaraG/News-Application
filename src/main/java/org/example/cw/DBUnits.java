ad1a928d6401e0af67191ab7997b26a92dd6a36e
Onaragamage <131785102+Onaragamage@users.noreply.github.com>
2024-11-15 23:58:34 +0530

    day 05

--- src/main/java/org/example/cw/DBUnits.java
+++
@@ -1,4 +0,0 @@
-package org.example.cw;
-
-public class DBUnits {
-}
--- src/main/java/org/example/cw/HomeController.java
+++ src/main/java/org/example/cw/HomeController.java
@@ -1,4 +1,36 @@
 package org.example.cw;
 
-public class HomeController {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+    @FXML
+    private Button button_log_out;
+    @FXML
+    private Label label_home;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        button_log_out.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "LogIn.fxml","Log In!",null);
+            }
+        });
+
+    }
+
+    public void setUserInformation(String username){
+        label_home.setText("Welcome"+username+"!");
+    }
 }
