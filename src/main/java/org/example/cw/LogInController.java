d1909d5da61b4e1c1014e1e9cd946032936afa5b
Onaragamage <131785102+Onaragamage@users.noreply.github.com>
2024-11-15 21:23:04 +0530

    day 05

--- src/main/java/org/example/cw/LogInController.java
+++ src/main/java/org/example/cw/LogInController.java
@@ -3,7 +3,7 @@ package org.example.cw;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LogInController {
     @FXML
     private Label welcomeText;
 
